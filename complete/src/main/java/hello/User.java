68218c004eb27b6e15cd2c1fcdaad052856a6332
Omi <omkar.9999@gmail.com>
2017-01-18 17:27:49 +0530

    :heavy_plus_sign: Password field

--- complete/src/main/java/hello/User.java
+++ complete/src/main/java/hello/User.java
@@ -15,6 +15,7 @@ public class User {
 	@Id private String id;
 
 	private String userName;
+	private String password;
 	private String firstName;
 	private String lastName;
 	private List<EndPoint> services;
@@ -50,5 +51,15 @@ public class User {
     public void setServices(List<EndPoint> services) {
         this.services = services;
     }
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
 	
 }
